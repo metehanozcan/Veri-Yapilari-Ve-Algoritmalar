fcc1b9e2115d86f00b02fe6d92abf47c82860d49
metehanozcan <ozcnmetehan@gmail.com>
2022-06-10 21:37:22 +0300

    Graph

--- 8-Graph.py
+++ 8-Graph.py
@@ -3,3 +3,10 @@
     Implementation of  a graph data structure
     """
 __author__="Metehan Özcan"
+
+from collections import defaultdict
+class Graph:
+    
+    def __init__(self,vertex):
+        self.V=vertex
+        self.graph =defaultdict(list)
