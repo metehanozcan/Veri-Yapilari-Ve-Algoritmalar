0f6ca460cba5b62b393d2411a83137793ca1cb4d
metehanozcan <ozcnmetehan@gmail.com>
2022-06-18 17:52:00 +0300

    Graph AdjList DFS and BFS Implementation

--- 8.1-Graph-AdjList.py
+++ 8.1-Graph-AdjList.py
@@ -1,46 +1,137 @@
-# Adjascency List representation in Python
+import queue
+import copy
 
+class DirectedGraph(object):
+    """
+    directed graph, with adj list
+    """
+    def __init__(self) :
+        self.adjacency_list={}
+        
+    def add_edge(self,source,destination):
+        """_summary_
+        adds an edge defined by vertices source and destination
+        
+        Args:
+            source (_type_): adds an edge defined by vertices source and destination
+            destination (_type_): _description_
+        """
+        
+        if source not in self.adjacency_list:
+            self.adjacency_list[source] = set()
+        self.adjacency_list[source].add(destination)
+    
+    def get_vertex(self):   
+        
+        """
+        Generator for returning the next vertex from the adjacency list
+        :return:
+        """     
+        for v in self.adjacency_list:
+            yield v
+    def get_neighbor(self,vertex):
+        """
+        Generator for returning vertex's neighbors to the given vertex
 
-class AdjNode:
-    def __init__(self, value):
-        self.vertex = value
-        self.next = None
+        Args:
+            vertex (_type_): _description_
 
+        Yields:
+            _type_: _description_
+        """
+        
+        if vertex in self.adjacency_list:
+            for u in self.adjacency_list[vertex]:
+                yield u
+                
+                
+    
+    def get_reverse_neighbor(self,vertex): 
+        if vertex in self.adjacency_list:
+            r_stack=[]
+            for i in self.get_neighbor(vertex):
+                r_stack.append(i)        
+            r_stack=r_stack[::-1]
+            for i in r_stack:            
+                yield i
+    def dfs(self):
+        seen = {}
+        vertexcantbereached = set()
+        to_visit = []
 
-class Graph:
-    def __init__(self, num):
-        self.V = num
-        self.graph = [None] * self.V
+        for vertex in self.get_vertex():
+            if vertex not in seen:
+                vertexcantbereached.add(vertex)
+            else:
+                continue
 
-    # Add edges
-    def add_edge(self, s, d):
-        node = AdjNode(d) #* Create a new node
-        node.next = self.graph[s]  #*
-        self.graph[s] = node #* Add the node to the adjacency list of source
+            to_visit.append(vertex)
 
-        node = AdjNode(s)
-        node.next = self.graph[d]
-        self.graph[d] = node
+            while to_visit:
+                v = to_visit.pop()
 
-    # Print the graph
-    def print_agraph(self):
-        for i in range(self.V):
-            print("Vertex " + str(i) + ":", end="")
-            temp = self.graph[i]
-            while temp:
-                print(" -> {}".format(temp.vertex), end="")
-                temp = temp.next
-            print(" \n")
+                for neighbor in self.get_neighbor(v):
+                    if neighbor not in seen:
+                        seen[neighbor] = v
+                        to_visit.append(neighbor)
+
+        return vertexcantbereached, seen
+        
+    def bfs(self):
+        seen={}
+        to_visit= queue.Queue()
+        for vertex in self.get_vertex():
+            to_visit.put(vertex)
+            
+            while not to_visit.empty():
+                v=to_visit.get()
+                
+                for neighbor in self.get_neighbor(v):
+                    if neighbor not in seen:
+                        seen[neighbor]=v
+                        to_visit.put(neighbor)
+        return seen
+                
+        
+    def listprinter(self):
+        print(self.adjacency_list)    
+        
+def get_test_graph_1():
+    dg = DirectedGraph()
+    dg.add_edge(0, 1)
+    dg.add_edge(0, 5)
+    dg.add_edge(1, 2)
+    dg.add_edge(2, 4)
+    dg.add_edge(2, 6)
+    dg.add_edge(3, 2)
+    dg.add_edge(5, 8)
+    dg.add_edge(6, 5)
+    dg.add_edge(7, 5)
+    
+
+    return dg
+def test_dfs():
+    
+    dg1 = get_test_graph_1()
+    c1, p1 = dg1.dfs()
+    assert (c1 == {0, 3, 7})
+    assert (p1 == {1: 0, 2: 1, 4: 2, 5: 0, 6: 2, 8: 5})
+def main():
+    dg=get_test_graph_1()
+    dg.listprinter()
+    print("DFS",dg.dfs())
+    print("BFS",dg.bfs())
+    # print(dg.get_neighbor(0))
+    # print(dg.get_reverse_neighbor(0))
+    test_dfs()
+    # test_dfs()
+    # test_bfs()
+    # test_contains_cycle()
+    # test_topological_sort()
+    # test_strongly_connected_components()
+
+    print("Tests complete.")
 
 
 if __name__ == "__main__":
-    V = 5
-
-    # Create graph and edges
-    graph = Graph(V)
-    graph.add_edge(0, 1)
-    graph.add_edge(0, 2)
-    graph.add_edge(0, 3)
-    graph.add_edge(1, 2)
-
-    graph.print_agraph()
+    main()
