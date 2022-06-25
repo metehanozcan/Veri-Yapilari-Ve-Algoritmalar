f08f41e28159f849d19649707ff7594f7f80e684
metehanozcan <ozcnmetehan@gmail.com>
2022-06-25 18:41:28 +0300

    Trie Implementation

---
+++ 9-Trie.py
@@ -0,0 +1,14 @@
+class Trie:
+    
+    def __init__(self) -> None:
+        self.word=False
+        self.children={}
+    
+    def insert(self,word:str)-> None:
+    
+    
+    def search(self,word)->bool:
+    
+    
+    def startsWith(self,prefix)->bool:
+    
