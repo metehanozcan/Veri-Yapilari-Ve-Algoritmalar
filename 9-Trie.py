574b06531063bcca9e64b98b2ea2bf19ea72f3b6
metehanozcan <ozcnmetehan@gmail.com>
2022-06-26 23:13:02 +0300

    Trie Implementation

--- 9-Trie.py
+++ 9-Trie.py
@@ -1,14 +1,68 @@
+class TrieNode:
+        # Initialize your data structure here.
+        def __init__(self):
+            self.word=False
+            self.children={}
+    
 class Trie:
-    
-    def __init__(self) -> None:
-        self.word=False
-        self.children={}
-    
-    def insert(self,word:str)-> None:
+
+    def __init__(self):
+        self.root = TrieNode()
     
     
-    def search(self,word)->bool:
+    def insert(self, word):
+        """Inserts word in trie.
+
+        Args:
+            word (string): param
+            returns None
+            
+            
+        """
+        node=self.root
+        for i in word:
+            if i not in node.children:
+                node.children[i]=TrieNode()
+            node=node.children[i]
+        node.word=True
+
     
+    def search(self, word):
+        """
+        Args:
+            word (string): param
+
+        Returns:
+            Boolean: returns if the word is in trie.            
+        """
+        
+        node=self.root
+        for i in word:
+            if i not in node.children:
+                return False
+            node=node.children[i]
+        return node.word
+
     
-    def startsWith(self,prefix)->bool:
+    def startsWith(self, prefix):
+        """
+
+        Args:
+            prefix (string): param
+
+        Returns:
+            boolean: returns if there is any word in trie starts with the given prefix
+        """
+        node=self.root
+        for i in prefix:
+            if i not in node.children:
+                return False
+            node=node.children[i]
+        return True
+            
     
+# Trie  instantiated 
+trie = Trie()
+print(trie.insert("test")) # insert
+print(trie.search("test")) #---> True
+print(trie.startsWith("a")) #---> False
